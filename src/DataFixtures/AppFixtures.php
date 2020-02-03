02440a1c482baa3f1dcea0a57bc4dff7eb29f207
malickdkt <thebestcrac@gmail.com>
2020-02-03 15:28:15 +0000

    appfixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,44 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Profil;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+     
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $role = new Profil();
+        $role ->setLibelle("Admin_system");
+        $manager->persist($role);
+
+        $role1 = new Profil();
+        $role1 ->setLibelle("Admin");
+        $manager->persist($role1);
+
+        $role2 = new Profil();
+        $role2 ->setLibelle("Cassier");
+        $manager->persist($role2);
+
+        //
+        $role3 = new Profil();
+        $role3 ->setLibelle("Partenaire");
+        $manager->persist($role3);
+        $admin = new User();
+        $admin->setUsername('malickdkt');
+        $admin->setLogin('diakhate@gmail.com');
+        $admin->setPassword($this->encoder->encodePassword($admin, "miko1234"));
 
         $manager->flush();
     }
